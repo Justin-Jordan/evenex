a1afbf74e4aac49b6e7eb12d2fbdf2b08c225ccf
justin <justinonomo@gmail.com>
2022-12-14 11:57:16 +0100

    add Attend Event section

---
+++ src/Plugin/Layout/Sections/EvenexAttendEventSection.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\evenex\Plugin\Layout\Sections;
+
+use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
+use Drupal\formatage_models\FormatageModelsThemes;
+use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
+
+/**
+ * Evenex Attend Event section
+ * @Layout(
+ *  id = "evenex_attend_event_section",
+ *  label = @Translation("Evenex: Attend event section"),
+ *  category = @Translation("evenex"),
+ *  path = "layouts/sections",
+ *  template = "attend_event_section",
+ *  library = "evenex/attend_event_section",
+ *  default_region = "title",
+ *  regions = {
+ *      "title" = {
+ *       "label" = @Translation("Evenex: title region"),
+ *      },
+ *      "description" = {
+ *       "label" = @Translation("Evenex: description region"),
+ *      },
+ *      "benefices" = {
+ *       "label" = @Translation("Evenex: benefices region"),
+ *      },
+ *  }
+ * )
+ */
+
+class EvenexAttendEventSection extends FormatageModelsSection
+{
+
+    /**
+     * {@inheritdoc}
+     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
+     */
+    public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager)
+    {
+        // TODO auto-generated method stub
+        parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/sections/attend_event.png");
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::build
+     */
+    public function build(array $regions)
+    {
+        // TODO auto-generated method stub
+        $build = parent::build($regions);
+        FormatageModelsThemes::formatSettingValues($build);
+
+        return $build;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function defaultConfiguration()
+    {
+        return parent::defaultConfiguration() + [
+            'css' => '',
+            'load_library' => true,
+            'region_css_title' => 'h1',
+            'content' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Layout informations',
+                    'loader' => 'static',
+                ],
+                'fields' => [
+                    'title' => [
+                        'text_html' => [
+                            'label' => 'Title',
+                            'value' => '',
+                        ]
+                    ],
+                    'description' => [
+                        'text_html' => [
+                            'label' => 'Description',
+                            'value' => '',
+                        ]
+                    ],
+                    'benefices' => [
+                        'text_html' => [
+                            'label' => 'Benefices',
+                            'value' => '',
+                        ]
+                    ],
+                ],
+            ],
+        ];
+    }
+}
