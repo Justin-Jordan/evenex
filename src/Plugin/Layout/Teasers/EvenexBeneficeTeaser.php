fc4a508502a4acf9980508595b3b5c5c86c0757e
justin <justinonomo@gmail.com>
2022-12-14 11:23:48 +0100

    add beneffice teaser

---
+++ src/Plugin/Layout/Teasers/EvenexBeneficeTeaser.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\evenex\Plugin\Layout\Teasers;
+
+use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
+use Drupal\formatage_models\FormatageModelsThemes;
+use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
+
+/**
+ * Evenex benefice teaser
+ *
+ * @Layout(
+ *   id = "evenex_benefice_teaser",
+ *   label = @Translation(" Evenex : Benefice teaser "),
+ *   category = @Translation("evenex"),
+ *   path = "layouts/teasers",
+ *   template = "benefice_teaser",
+ *   library = "evenex/benefice_teaser",
+ *   default_region = "title",
+ *   regions = {
+ *     "icon" = {
+ *       "label" = @Translation("icon"),
+ *     },
+ *     "title" = {
+ *       "label" = @Translation("title"),
+ *     },
+ *     "description" = {
+ *       "label" = @Translation("description"),
+ *     },
+ *   }
+ * )
+ */
+
+class EvenexBeneficeTeaser extends FormatageModelsSection
+{
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager)
+    {
+        // TODO Auto-generated method stub
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/teasers/benefice_teaser.png");
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
+     */
+    public function build(array $regions)
+    {
+        // TODO Auto-generated method stub
+        $build = parent::build($regions);
+        FormatageModelsThemes::formatSettingValues($build);
+        return $build;
+    }
+
+    /**
+     * 
+     * {@inheritdoc}
+     * 
+     */
+    function defaultConfiguration()
+    {
+        return [
+            'load_libray' => true,
+            'region_css_title' => 'h4',
+            'infos' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Layout informations',
+                    'loader' => 'static'
+                ],
+                'fields' => [
+                    'icon' => [
+                        'text_html' => [
+                            'label' => 'Icon',
+                            'value' => ''
+                        ]
+                    ],
+                    'title' => [
+                        'text' => [
+                            'label' => 'Title',
+                            'value' => ''
+                        ]
+                    ],
+                    'description' => [
+                        'text_html' => [
+                            'label' => 'Description',
+                            'value' => ''
+                        ]
+                    ],
+                ]
+            ]
+        ] + parent::defaultConfiguration();
+    }
+}
