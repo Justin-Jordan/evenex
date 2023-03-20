6ffb3755b4ad44e277fc10da08aec12bcc57d61e
kouwa stephane <kksasteph888@gmail.com>
2023-03-20 09:20:48 +0100

    Maj

--- src/Plugin/Layout/Teasers/EvenexAfterPartyElement.php
+++ src/Plugin/Layout/Teasers/EvenexAfterPartyElement.php
@@ -4,7 +4,7 @@ namespace Drupal\evenex\Plugin\Layout\Teasers;
 
 use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
 use Drupal\formatage_models\FormatageModelsThemes;
-use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
+use Drupal\formatage_models\Plugin\Layout\Teasers\FormatageModelsTeasers;
 
 /**
  * Evenex after party teaser
@@ -33,84 +33,80 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *   }
  * )
  */
-
-class EvenexAfterPartyElement extends FormatageModelsSection
-{
-
-    /**
-     *
-     * {@inheritdoc}
-     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
-     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager)
-    {
-        // TODO Auto-generated method stub
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/teasers/evenex_after_party_element.png");
-    }
-
-    /**
-     *
-     * {@inheritdoc}
-     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
-     */
-    public function build(array $regions)
-    {
-        // TODO Auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-        return $build;
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     * 
-     */
-    function defaultConfiguration()
-    {
-        return [
-            'load_libray' => true,
-            'region_css_tilte' => 'h4',
-            "derivate" => [
-                'value' => '',
-                'options' => [
-                    'evenex-ap--instagram' => 'instagram',
-                ],
-            ],
-            'infos' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' => 'Layout informations',
-                    'loader' => 'static'
-                ],
-                'fields' => [
-                    'title' => [
-                        'text' => [
-                            'label' => 'Title',
-                            'value' => ''
-                        ]
-                    ],
-                    'subtitle' => [
-                        'text_html' => [
-                            'label' => 'Description',
-                            'value' => ''
-                        ]
-                    ],
-                    'image' => [
-                        'text_html' => [
-                            'label' => 'Image',
-                            'value' => ''
-                        ]
-                    ],
-                    'icon' => [
-                        'text_html' => [
-                            'label' => 'Icon',
-                            'value' => ''
-                        ]
-                    ],
-                ]
+class EvenexAfterPartyElement extends FormatageModelsTeasers {
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
+    // TODO Auto-generated method stub
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/teasers/evenex_after_party_element.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build()
+   */
+  public function build(array $regions) {
+    // TODO Auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   *
+   */
+  function defaultConfiguration() {
+    return [
+      'load_libray' => true,
+      'region_css_tilte' => 'h4',
+      "derivate" => [
+        'value' => '',
+        'options' => [
+          'evenex-ap--instagram' => 'instagram'
+        ]
+      ],
+      'infos' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Layout informations',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'title' => [
+            'text' => [
+              'label' => 'Title',
+              'value' => ''
             ]
-        ] + parent::defaultConfiguration();
-    }
+          ],
+          'subtitle' => [
+            'text_html' => [
+              'label' => 'Description',
+              'value' => ''
+            ]
+          ],
+          'image' => [
+            'text_html' => [
+              'label' => 'Image',
+              'value' => ''
+            ]
+          ],
+          'icon' => [
+            'text_html' => [
+              'label' => 'Icon',
+              'value' => ''
+            ]
+          ]
+        ]
+      ]
+    ] + parent::defaultConfiguration();
+  }
+  
 }
