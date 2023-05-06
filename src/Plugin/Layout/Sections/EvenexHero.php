8a91eea1bc515ab8301eed759634562a9819dba9
kouwa stephane <kksasteph888@gmail.com>
2023-05-06 18:36:50 +0200

    Maj

--- src/Plugin/Layout/Sections/EvenexHero.php
+++ src/Plugin/Layout/Sections/EvenexHero.php
@@ -8,6 +8,7 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 
 /**
  * Evenex hero section
+ *
  * @Layout(
  *  id = "evenex-hero",
  *  label = @Translation("Evenex: Hero"),
@@ -18,138 +19,143 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *  default_region = "title",
  *  regions = {
  *      "title" = {
- *       "label" = @Translation("Evenex: title region"),
+ *       "label" = @Translation("title region"),
  *      },
  *      "subTitle" = {
- *       "label" = @Translation("Evenex: subtitle region"),
+ *       "label" = @Translation("subtitle region"),
  *      },
  *      "date" = {
- *       "label" = @Translation("Evenex: date region"),
+ *       "label" = @Translation("date region"),
  *      },
  *      "callToActionIcon" = {
- *       "label" = @Translation("Evenex: call to action icon region"),
+ *       "label" = @Translation("call to action icon region"),
  *      },
  *      "callToAction" = {
- *       "label" = @Translation("Evenex: call to action region"),
+ *       "label" = @Translation("call to action region"),
  *      },
  *      "by" = {
- *       "label" = @Translation("Evenex: by region"),
+ *       "label" = @Translation("by region"),
  *      },
  *      "secondadyBackgroundImage" = {
- *       "label" = @Translation("Evenex: secondary background image region"),
+ *       "label" = @Translation("secondary background image region"),
  *      },
  *      "mainBackgroundImage" = {
- *       "label" = @Translation("Evenex: main background image region"),
+ *       "label" = @Translation("main background image region"),
  *      },
  *      "location" = {
- *       "label" = @Translation("Evenex: location region"),
+ *       "label" = @Translation("location region"),
  *      },
- *      
+ *      "first_image" = {
+ *       "label" = @Translation("First image"),
+ *      },
+ *      "second_image" = {
+ *       "label" = @Translation("Second image"),
+ *      },
+ *
  *  }
  * )
  */
-
-class EvenexHero extends FormatageModelsSection
-{
-
-    /**
-     * {@inheritdoc}
-     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
-     */
-    public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager)
-    {
-        // TODO auto-generated method stub
-        parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/sections/evenex_hero.png");
-    }
-
-    /**
-     * {@inheritdoc}
-     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::build
-     */
-    public function build(array $regions)
-    {
-        // TODO auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-
-        return $build;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        return parent::defaultConfiguration() + [
-            'css' => '',
-            'load_library' => true,
-            'region_css_location' => 'h2',
-            'region_css_by' => 'h4',
-            'region_css_date' => 'h4',
-            'content' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' => 'Layout informations',
-                    'loader' => 'static',
-                ],
-                'fields' => [
-                    'title' => [
-                        'text_html' => [
-                            'label' => 'Title',
-                            'value' => '',
-                        ]
-                    ],
-                    'subTitle' => [
-                        'text_html' => [
-                            'label' => 'SubTitle',
-                            'value' => '',
-                        ]
-                    ],
-                    'callToActionIcon' => [
-                        'text_html' => [
-                            'label' => 'Call To Action Icon',
-                            'value' => '',
-                        ]
-                    ],
-                    'callToAction' => [
-                        'text_html' => [
-                            'label' => 'Call To Action',
-                            'value' => '',
-                        ]
-                    ],
-                    'secondadyBackgroundImage' => [
-                        'text_html' => [
-                            'label' => 'Secondary Background Image',
-                            'value' => '',
-                        ]
-                    ],
-                    'by' => [
-                        'text_html' => [
-                            'label' => 'By',
-                            'value' => '',
-                        ]
-                    ],
-                    'mainBackgroundImage' => [
-                        'text_html' => [
-                            'label' => 'Main Background Image',
-                            'value' => '',
-                        ]
-                    ],
-                    'location' => [
-                        'text_html' => [
-                            'label' => 'Location',
-                            'value' => '',
-                        ]
-                    ],
-                    'date' => [
-                        'text_html' => [
-                            'label' => 'Date',
-                            'value' => '',
-                        ]
-                    ],
-                ],
-            ],
-        ];
-    }
+class EvenexHero extends FormatageModelsSection {
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
+   */
+  public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager) {
+    // TODO auto-generated method stub
+    parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/sections/evenex_hero.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build
+   */
+  public function build(array $regions) {
+    // TODO auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'css' => 'd-flex justify-content-center align-items-center text-white',
+      'load_library' => true,
+      'region_css_location' => 'h2',
+      'region_css_by' => 'h4',
+      'region_css_date' => 'h4',
+      'content' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Layout informations',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'title' => [
+            'text_html' => [
+              'label' => 'Title',
+              'value' => ''
+            ]
+          ],
+          'subTitle' => [
+            'text_html' => [
+              'label' => 'SubTitle',
+              'value' => ''
+            ]
+          ],
+          'callToActionIcon' => [
+            'text_html' => [
+              'label' => 'Call To Action Icon',
+              'value' => ''
+            ]
+          ],
+          'callToAction' => [
+            'text_html' => [
+              'label' => 'Call To Action',
+              'value' => ''
+            ]
+          ],
+          'secondadyBackgroundImage' => [
+            'text_html' => [
+              'label' => 'Secondary Background Image',
+              'value' => ''
+            ]
+          ],
+          'by' => [
+            'text_html' => [
+              'label' => 'By',
+              'value' => ''
+            ]
+          ],
+          'mainBackgroundImage' => [
+            'text_html' => [
+              'label' => 'Main Background Image',
+              'value' => ''
+            ]
+          ],
+          'location' => [
+            'text_html' => [
+              'label' => 'Location',
+              'value' => ''
+            ]
+          ],
+          'date' => [
+            'text_html' => [
+              'label' => 'Date',
+              'value' => ''
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+  
 }
--- src/Plugin/Layout/Sections/EvenexPricingSection.php
+++ src/Plugin/Layout/Sections/EvenexPricingSection.php
@@ -8,6 +8,7 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
 
 /**
  * Evenex Pricing section
+ *
  * @Layout(
  *  id = "evenex_pricing_section",
  *  label = @Translation("Evenex: Pricing section"),
@@ -18,91 +19,96 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *  default_region = "title",
  *  regions = {
  *      "title" = {
- *       "label" = @Translation("Evenex: title region"),
+ *       "label" = @Translation("title region"),
  *      },
  *      "tickets" = {
- *       "label" = @Translation("Evenex: tickets region"),
+ *       "label" = @Translation("tickets region"),
  *      },
  *      "backgroundImage" = {
- *       "label" = @Translation("Evenex: background image region"),
+ *       "label" = @Translation("background image region"),
  *      },
  *      "terms" = {
- *       "label" = @Translation("Evenex: terms region"),
+ *       "label" = @Translation("terms region"),
  *      },
- *      
+ *      "icon_left" = {
+ *       "label" = @Translation("icon_left"),
+ *      },
+ *      "icon_right" = {
+ *       "label" = @Translation("icon_right"),
+ *      },
+ *
  *  }
  * )
  */
-
-class EvenexPricingSection extends FormatageModelsSection
-{
-
-    /**
-     * {@inheritdoc}
-     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
-     */
-    public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager)
-    {
-        // TODO auto-generated method stub
-        parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/sections/pricing.png");
-    }
-
-    /**
-     * {@inheritdoc}
-     * @see Drupal\formatage_models\Plugin\Layout\FormatageModels::build
-     */
-    public function build(array $regions)
-    {
-        // TODO auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-
-        return $build;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        return parent::defaultConfiguration() + [
-            'css' => '',
-            'load_library' => true,
-            'region_css_title' => 'h1',
-            'content' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' => 'Layout informations',
-                    'loader' => 'static',
-                ],
-                'fields' => [
-                    'title' => [
-                        'text' => [
-                            'label' => 'Title',
-                            'value' => 'Latest from Newsroom',
-                        ]
-                    ],
-                    'tickets' => [
-                        'text_html' => [
-                            'label' => 'Tickets',
-                            'value' => '',
-                        ]
-                    ],
-                    'backgroundImage' => [
-                        'text_html' => [
-                            'label' => 'Background Image',
-                            'value' => ''
-                        ]
-                    ],
-                    'terms' => [
-                        'text_html' => [
-                            'label' => 'terms',
-                            'value' => ''
-                        ]
-                    ],
-                ],
-            ],
-        ];
-    }
+class EvenexPricingSection extends FormatageModelsSection {
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::_construct
+   */
+  public function __construct(array $configuration, $pludin_id, $plugin_definition, StylesGroupManager $styleGroupManager) {
+    // TODO auto-generated method stub
+    parent::__construct($configuration, $pludin_id, $plugin_definition, $styleGroupManager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'evenex') . "/icones/sections/pricing.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::build
+   */
+  public function build(array $regions) {
+    // TODO auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'css' => '',
+      'load_library' => true,
+      'region_css_title' => 'h1',
+      'content' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Layout informations',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'title' => [
+            'text' => [
+              'label' => 'Title',
+              'value' => 'Latest from Newsroom'
+            ]
+          ],
+          'tickets' => [
+            'text_html' => [
+              'label' => 'Tickets',
+              'value' => ''
+            ]
+          ],
+          'backgroundImage' => [
+            'text_html' => [
+              'label' => 'Background Image',
+              'value' => ''
+            ]
+          ],
+          'terms' => [
+            'text_html' => [
+              'label' => 'terms',
+              'value' => ''
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+  
 }
